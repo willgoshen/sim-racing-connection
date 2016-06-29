8296fa1e5270cba790c606476f02d175b7f9f44c
willgoshen <willgoshen@gmail.com>
2016-06-28 22:53:00 -0400

    server.js
    Updated server.js to make it a little more verbose in command line

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ var app = connect();
 
 app.use(serveStatic('../sim-racing-connection'));
 app.listen(8000);
+console.log("Listening on port 8000");
+console.log("Access site at http://localhost:8000/index.html")
